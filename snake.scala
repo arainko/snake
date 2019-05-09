7998daac25102a03306d73a3b3af9b0e471592ec
arainko <46346508+arainko@users.noreply.github.com>
2019-05-10 00:47:36 +0200

    let's just say it kinda works

--- snake.scala
+++ snake.scala
@@ -1,7 +1,16 @@
 import scala.collection.immutable.{Vector => Vec}
 
-class Snake{
-    ???
+class Snake(headCord: (Int, Int), bodyCords: Seq[(Int, Int)], direction: String) {
+    def body: Seq[(Int, Int)] = bodyCords
+    def head: (Int, Int) = headCord
+    def dir: String = direction
+
+    def next: Snake = direction match {
+        case "left" => new Snake( (headCord._1-1, headCord._2), headCord +: bodyCords.init, direction )
+        case "up" => new Snake( (headCord._1, headCord._2-1), headCord +: bodyCords.init, direction )
+        case "right" => new Snake( (headCord._1+1, headCord._2), headCord +: bodyCords.init, direction )
+        case "down" => new Snake( (headCord._1, headCord._2+1), headCord +: bodyCords.init, direction )
+    }
 }
 
 class Apple() {
@@ -13,7 +22,11 @@ class Game(h: Int, w: Int) {
     val height = h
     val width = w
 
-    def gameBoard(targetRows: Int, targetCols: Int): Vec[Vec[Char]] = {
+    val initSnakeBody = Seq((14,5), (15,5), (16,5))
+    val initSnake = new Snake( (13,5), initSnakeBody, "up")
+    val emptyBoard = initialBoard(height, width)
+
+    private def initialBoard(targetRows: Int, targetCols: Int): Vec[Vec[Char]] = {
         val empty2D = Vec.empty[Vec[Char]]
         val empty1D = Vec.empty[Char]
         val fillChar = ' '
@@ -27,18 +40,37 @@ class Game(h: Int, w: Int) {
         helper(0, 0, empty1D, empty2D)
     }
 
-    def render: Unit = {
-        val board = gameBoard(height, width)
-        def prettyPrint: Unit = println("+" + "-" * width + "+") 
+    private def snakeBoard(snake: Snake, board: Vec[Vec[Char]]): Vec[Vec[Char]] = {
+        val bodylessBoard = board.updated(snake.head._2,
+                            board(snake.head._2).updated(snake.head._1, 'X'))
 
-        prettyPrint
-        board.foreach(n => println("|" + n.mkString("") + "|" ))
-        prettyPrint
+        def helper(snakePosSeq: Seq[(Int, Int)], output: Vec[Vec[Char]]): Vec[Vec[Char]] = snakePosSeq match {
+            case Seq() => output
+            case Seq(head, tail @ _*) => helper(tail, output.updated(head._2, 
+                                                      output(head._2).updated(head._1, '0')))
+        }
+        helper(snake.body, bodylessBoard)
+    }
+
+    def render: Unit = {
+        def prettyPrint: Unit = println("+" + "-" * width + "+")
+        def helper(snake: Snake): Unit = {
+            print("\u001b[2J")
+            val board = snakeBoard(snake, emptyBoard)
+            // --- --- ---
+            prettyPrint
+            board.foreach(n => println("|" + n.mkString("") + "|" ))
+            prettyPrint
+            // --- --- ---
+            Thread.sleep(500)
+            helper(snake.next)
+        }
+        helper(initSnake)
     }
 
 }
 
 object SnakeGame extends App {
-    val a = new Game(10,30)
-    a.render
+    val game = new Game(10,30)
+    game.render
 }
